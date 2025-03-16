9c9bef5ff0346515db2388a22473bdf16e19b8ac
jeansparent <jeanseb.parent@outlook.com>
2025-03-16 10:31:50 -0400

    script pour docker

---
+++ Bash/Docker/sysbench.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Auteur : Jean-Sébastien Parent
+# Date: 16 mars 2025
+
+help=false
+
+# Options du script
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --version) 
+            if [[ -n "$2" && ! "$2" =~ ^-- ]]; then
+                VERSION="$2"
+                shift  
+            else
+                echo "Erreur : --version nécessite une valeur (ex: --version 1.0)"
+                exit 1
+            fi
+            ;;
+        --help) help=true ;;
+        --) shift; break ;;
+        *) echo "Option inconnue : $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if $help; then
+    echo "Usage: $0 [--env <valeur>] [--help]"
+    echo "  --version val    Spécifie la version"
+    echo "  --help         Affiche cette aide"
+    exit 0
+fi
+
+docker build -t jseb00/sysbench:$VERSION -t jseb00/sysbench:latest /home/administrateur/inf1430/Docker/sysbench/
+
+docker login
+
+docker push jseb00/sysbench:$VERSION
+docker push jseb00/sysbench:latest
