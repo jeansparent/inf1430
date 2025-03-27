b48be632a0f60d5ee548da247e469da95b55cccf
jeansparent <jeanseb.parent@outlook.com>
2025-03-27 16:54:46 -0400

    MaJ script

--- Bash/Test/Reseaux/reseaux_vm.sh
+++ Bash/Test/Reseaux/reseaux_vm.sh
@@ -1,10 +1,17 @@
 # Start iperf3
+echo "Starting iperf3 in deamon mode remotly on vm-reseau-vm.inf1430"
 ssh administrateur@vm-reseau-vm.inf1430 'iperf3 -s -D'
+echo ""
+echo ""
 
 # iperf for udp
+echo "Starting udp test with vm-reseau-vm.inf1430"
 iperf3 -c vm-reseau-vm.inf1430 -u -b 0 -n 5G -f m
+echo ""
+echo ""
 
 # iperf for tcp
+echo "Starting tcp test with vm-reseau-vm.inf1430"
 iperf3 -c vm-reseau-vm.inf1430 -n 5G -b 0 -f m
-
-
+echo ""
+echo ""
