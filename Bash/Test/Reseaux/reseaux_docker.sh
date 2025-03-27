1decde633717603eab19615e70ca3f50b3673373
jeansparent <jeanseb.parent@outlook.com>
2025-03-27 17:07:36 -0400

    MaJ Script

--- Bash/Test/Reseaux/reseaux_docker.sh
+++ Bash/Test/Reseaux/reseaux_docker.sh
@@ -1,13 +1,15 @@
+FQDN='vm-reseau-docker.inf1430'
+
 # iperf for udp
-echo "Starting udp test with vm-reseau-vm.inf1430"
+echo "Starting udp test with $FQDN"
 echo ""
-iperf3 -c vm-reseau-vm.inf1430 -u -b 0 -n 5G -f m
+iperf3 -c $FQDN -u -b 0 -n 5G -f m
 echo ""
 echo "******************************************"
 
 # iperf for tcp
-echo "Starting tcp test with vm-reseau-vm.inf1430"
+echo "Starting tcp test with $FQDN"
 echo ""
-iperf3 -c vm-reseau-vm.inf1430 -n 5G -b 0 -f m
+iperf3 -c $FQDN -n 5G -b 0 -f m
 echo ""
 echo "******************************************"
--- Bash/Test/Reseaux/reseaux_vm.sh
+++ Bash/Test/Reseaux/reseaux_vm.sh
@@ -1,19 +1,27 @@
+#!/usr/bin/env bash
+
+# Auteur : Jean-Sébastien Parent
+# Date: 27 mars 2025
+# Ce script doit être exécuter à partir d'un instance bastion afin d'avoir test valide sans prendre Internet en compte.
+
+FQDN='vm-reseau-vm.inf1430'
+
 # Start iperf3
-echo "Starting iperf3 in deamon mode remotly on vm-reseau-vm.inf1430"
-ssh administrateur@vm-reseau-vm.inf1430 'iperf3 -s -D'
+echo "Starting iperf3 in deamon mode remotly on $FQDN"
+ssh administrateur@$FQDN 'iperf3 -s -D'
 echo ""
 echo "******************************************"
 
 # iperf for udp
-echo "Starting udp test with vm-reseau-vm.inf1430"
+echo "Starting udp test with $FQDN"
 echo ""
-iperf3 -c vm-reseau-vm.inf1430 -u -b 0 -n 5G -f m
+iperf3 -c $FQDN -u -b 0 -n 5G -f m
 echo ""
 echo "******************************************"
 
 # iperf for tcp
-echo "Starting tcp test with vm-reseau-vm.inf1430"
+echo "Starting tcp test with $FQDN"
 echo ""
-iperf3 -c vm-reseau-vm.inf1430 -n 5G -b 0 -f m
+iperf3 -c $FQDN -n 5G -b 0 -f m
 echo ""
 echo "******************************************"
