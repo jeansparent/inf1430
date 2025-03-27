536548602d81fbb7a9f8036792d095760ee2c65c
jeansparent <jeanseb.parent@outlook.com>
2025-03-27 16:59:43 -0400

    MaJ Script reseaux

--- Bash/Test/Reseaux/reseaux_docker.sh
+++ Bash/Test/Reseaux/reseaux_docker.sh
@@ -1,5 +1,13 @@
 # iperf for udp
-iperf3 -c vm-reseau-docker.inf1430 -u -b 0 -n 5G -f m
+echo "Starting udp test with vm-reseau-vm.inf1430"
+echo ""
+iperf3 -c vm-reseau-vm.inf1430 -u -b 0 -n 5G -f m
+echo ""
+echo "******************************************"
 
 # iperf for tcp
-iperf3 -c vm-reseau-docker.inf1430 -n 5G -b 0 -f m
+echo "Starting tcp test with vm-reseau-vm.inf1430"
+echo ""
+iperf3 -c vm-reseau-vm.inf1430 -n 5G -b 0 -f m
+echo ""
+echo "******************************************"
--- Bash/Test/Reseaux/reseaux_vm.sh
+++ Bash/Test/Reseaux/reseaux_vm.sh
@@ -2,16 +2,18 @@
 echo "Starting iperf3 in deamon mode remotly on vm-reseau-vm.inf1430"
 ssh administrateur@vm-reseau-vm.inf1430 'iperf3 -s -D'
 echo ""
-echo ""
+echo "******************************************"
 
 # iperf for udp
 echo "Starting udp test with vm-reseau-vm.inf1430"
+echo ""
 iperf3 -c vm-reseau-vm.inf1430 -u -b 0 -n 5G -f m
 echo ""
-echo ""
+echo "******************************************"
 
 # iperf for tcp
 echo "Starting tcp test with vm-reseau-vm.inf1430"
+echo ""
 iperf3 -c vm-reseau-vm.inf1430 -n 5G -b 0 -f m
 echo ""
-echo ""
+echo "******************************************"
