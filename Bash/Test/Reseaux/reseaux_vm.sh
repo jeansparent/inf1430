1bfe00ce9c92fbfbf6bf0b8e9e6a885e3dcbb87a
jeansparent <jeanseb.parent@outlook.com>
2025-03-28 07:26:02 -0400

    MaJ

--- Bash/Test/Reseaux/reseaux_vm.sh
+++ Bash/Test/Reseaux/reseaux_vm.sh
@@ -15,13 +15,13 @@ echo "******************************************"
 # iperf for udp
 echo "Starting udp test with $FQDN"
 echo ""
-iperf3 -c $FQDN -u -b 0 -n 5G -f m
+iperf3 -c $FQDN -u -b 0 -n 10G -f m
 echo ""
 echo "******************************************"
 
 # iperf for tcp
 echo "Starting tcp test with $FQDN"
 echo ""
-iperf3 -c $FQDN -n 5G -b 0 -f m
+iperf3 -c $FQDN -n 10G -b 0 -f m
 echo ""
 echo "******************************************"
