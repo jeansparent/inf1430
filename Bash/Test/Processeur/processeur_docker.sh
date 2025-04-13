b1961a740d2ec728d1211997e493f498a7f467a6
jeansparent <jeanseb.parent@outlook.com>
2025-04-13 10:49:29 -0400

    Modif Scripts processeur

--- Bash/Test/Processeur/processeur_docker.sh
+++ Bash/Test/Processeur/processeur_docker.sh
@@ -5,7 +5,6 @@
 # Ce script doit être exécuter à partir d'un instance bastion afin d'avoir test valide sans prendre Internet en compte.
 
 FQDN='vm-processeur-docker.inf1430'
-size='100G'
 sysbench_repo='jseb00/sysbench'
 docker_number=$RANDOM
 docker_name="sysbench_$docker_number"
@@ -22,14 +21,14 @@ for i in {1..5}; do
     # 1 cpu
     echo "Starting cpu test for 1 core #$i with $FQDN"
     echo ""
-    ssh -t administrateur@$FQDN "docker exec $docker_name sysbench cpu --threads=1 --time=$time run"
+    ssh -t administrateur@$FQDN "docker exec $docker_name sysbench cpu --threads=1 --cpu-max-prime=20000 --time=$time run"
     echo ""
     echo "******************************************"
 
     # all cpu
     echo "Starting cpu test for $threads cores #$i with $FQDN"
     echo ""
-    ssh -t administrateur@$FQDN "docker exec $docker_name sysbench cpu --threads=$threads --time=$time run"
+    ssh -t administrateur@$FQDN "docker exec $docker_name sysbench cpu --threads=$threads --cpu-max-prime=20000 --time=$time run"
     echo ""
     echo "******************************************"
 
