0e2a5c852e887a5ce4bc018956309bdba0cd74fd
jeansparent <jeanseb.parent@outlook.com>
2025-04-23 16:38:45 -0400

    script de build pour python-frontend

---
+++ Bash/Docker/python-frontend.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Auteur : Jean-Sébastien Parent
+# Date: 23 avril 2025
+
+docker_repo="jseb00"
+help=false
+
+# Options du script
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --version) 
+            if [[ -n "$2" && ! "$2" =~ ^-- ]]; then
+                VERSION="$2"
+                shift  
+            else
+                echo "Erreur : --version nécessite une valeur (ex: --version 1.0)"
+                exit 1
+            fi
+            ;;
+        --help) help=true ;;
+        --) shift; break ;;
+        *) echo "Option inconnue : $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if $help; then
+    echo "Usage: $0 [--env <valeur>] [--help]"
+    echo "  --version val    Spécifie la version"
+    echo "  --help         Affiche cette aide"
+    exit 0
+fi
+
+docker build -t $docker_repo/python-frontend:$VERSION -t $docker_repo/python-frontend:latest ./Python/python-frontend/
+
+docker login
+
+docker push $docker_repo/sysbench:$VERSION
+docker push $docker_repo/sysbench:latest
