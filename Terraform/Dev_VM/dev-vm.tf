0a849020e4cb0078507a94d63791d672fb25e3dd
jeansparent <jeanseb.parent@outlook.com>
2025-04-25 14:44:35 -0400

    updates

--- Terraform/Dev_VM/dev-vm.tf
+++ Terraform/Dev_VM/dev-vm.tf
@@ -89,9 +89,23 @@ resource "azurerm_network_security_rule" "nsg_dev-vm_ssh" {
   network_security_group_name = azurerm_network_security_group.nsg_dev-vm.name
 }
 
-resource "azurerm_network_security_rule" "nsg_dev-vm_http" {
+resource "azurerm_network_security_rule" "nsg_dev-vm_http_8080" {
   name                        = "HTTP"
   priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "8080"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg_dev-env.name
+  network_security_group_name = azurerm_network_security_group.nsg_dev-vm.name
+}
+
+resource "azurerm_network_security_rule" "nsg_dev-vm_http" {
+  name                        = "HTTP"
+  priority                    = 400
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "*"
