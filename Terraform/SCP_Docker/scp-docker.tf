8ec041a187664533a19593e245f95d6c37a5bd19
jeansparent <jeanseb.parent@outlook.com>
2025-04-26 09:35:26 -0400

    ajout port NSG

--- Terraform/SCP_Docker/scp-docker.tf
+++ Terraform/SCP_Docker/scp-docker.tf
@@ -78,6 +78,20 @@ resource "azurerm_network_security_rule" "nsg_scp_docker_ssh" {
   network_security_group_name = azurerm_network_security_group.nsg_scp_docker.name
 }
 
+resource "azurerm_network_security_rule" "nsg_scp_docker_ssh-session" {
+  name                        = "ssh-session"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "2222"
+  source_address_prefix       = "192.168.0.10/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg_scp_docker.name
+  network_security_group_name = azurerm_network_security_group.nsg_scp_docker.name
+}
+
 resource "azurerm_network_interface_security_group_association" "nsg_scp_docker_association" {
   network_interface_id      = azurerm_network_interface.vm_scp_docker_nic_1.id
   network_security_group_id = azurerm_network_security_group.nsg_scp_docker.id
