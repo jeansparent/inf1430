5aceeba02d8aa657b614eb3b11383e003ddcee7d
jeansparent <jeanseb.parent@outlook.com>
2025-05-03 12:31:45 -0400

    MaJ script de test

--- Bash/Test/Applicatif/sc1.sh
+++ Bash/Test/Applicatif/sc1.sh
@@ -9,6 +9,15 @@ help=false
 # Options du script
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
+        --ip) 
+            if [[ -n "$2" && ! "$2" =~ ^-- ]]; then
+                IP="$2"
+                shift
+            else
+                echo "Erreur : --ip nécessite une valeur (ex: --ip)"
+                exit 1
+            fi
+            ;;
         --url) 
             if [[ -n "$2" && ! "$2" =~ ^-- ]]; then
                 URL="$2"
@@ -36,11 +45,48 @@ done
 if $help; then
     echo "Usage: $0 [--ip <valeur>] [--help]"
     echo "  --url val    Spécifie l'url"
+    echo "  --ip val    Spécifie l'IP"
     echo "  --help         Affiche cette aide"
     exit 0
 fi
 
 for i in {1..5}; do
+    echo "Cleanup VM"
+    ssh -P 22 administrateur@$IP "rm -rf /home/administrateur/pid*.log"
+    rm -rf /home/administrateur/pid*.log
+
     echo "Test SC1 Page: $i concurent: 10 Total: 100"
     ab -n 100 -c 10 "$URL/CSV?records=1000&page=$i"
+
+    echo "Starting pidstat"
+    ssh -P 22 administrateur@$IP "nohup pidstat -u 1 > pidstat_cpu.log 2>&1 &"
+    pidstat_cpu_pid=$!
+    ssh -P 22 administrateur@$IP "nohup pidstat -r 1 > pidstat_mem.log 2>&1 &"
+    pidstat_mem_pid=$!
+
+    echo "Transfert pidstat file"
+    scp -P 22 administrateur@$IP:pidstat* ./
+
+    echo "Stop pidstat"
+    ssh -P 22 administrateur@$IP "pkill -f pistart"
+
+    echo "========== PIDSTAT REPORT for SSHD =========="
+    cpu_values=$(grep -a "postgres" pidstat_cpu.log | tr -s ' ' | cut -d ' ' -f 8)
+    cpu_number_value=$(echo "$cpu_values" | wc -l)
+    cpu_sum=$(echo "$cpu_values" | awk '{sum+=$1} END {print sum}')
+    cpu_mean=$(echo "$cpu_sum / $cpu_number_value" | bc -l)
+    cpu_max=$(echo "$cpu_values" | sort -nr | head -1)
+
+
+    mem_values=$(grep -a "postgres" pidstat_mem.log | tr -s ' ' | cut -d ' ' -f 7)
+    mem_number_value=$(echo "$mem_values" | wc -l)
+    mem_sum=$(echo "$mem_values" | awk '{sum+=$1} END {print sum}')
+    mem_mean=$(echo "$mem_sum / $mem_number_value" | bc -l)
+    mem_max=$(echo "$mem_values" | sort -nr | head -1)
+
+    echo "Max CPU usage for SSHD: $cpu_max %"
+    echo "Mean CPU usage for SSHD: $cpu_mean %"
+    echo "Max MEM (RSS) for SSHD: $mem_max KB"
+    echo "Mean MEM (RSS) for SSHD: $mem_mean KB"
+    echo "============================================="
 done
--- Bash/Test/Applicatif/sc4-7.sh
+++ Bash/Test/Applicatif/sc4-7.sh
@@ -9,6 +9,15 @@ help=false
 # Options du script
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
+        --ip) 
+            if [[ -n "$2" && ! "$2" =~ ^-- ]]; then
+                IP="$2"
+                shift
+            else
+                echo "Erreur : --ip nécessite une valeur (ex: --ip)"
+                exit 1
+            fi
+            ;;
         --url) 
             if [[ -n "$2" && ! "$2" =~ ^-- ]]; then
                 URL="$2"
@@ -41,7 +50,45 @@ if $help; then
 fi
 
 for i in {1..5}; do
-    echo "Test SC3 concurent: 10 Total: 5000"
-    ab -n 5000 -c 10 "$URL/DB-API?records=1000&page=$i"
+    echo "Cleanup VM"
+    ssh -P 22 administrateur@$IP "rm -rf /home/administrateur/pid*.log"
+    rm -rf /home/administrateur/pid*.log
+
+    echo "Starting pidstat"
+    ssh -P 22 administrateur@$IP "nohup pidstat -u 1 > pidstat_cpu.log 2>&1 &"
+    pidstat_cpu_pid=$!
+    ssh -P 22 administrateur@$IP "nohup pidstat -r 1 > pidstat_mem.log 2>&1 &"
+    pidstat_mem_pid=$!
+
+    echo "Test SC4-7 concurent: 10 Total: 5000"
+    ab -n 500 -c 10 "$URL/DB-API?records=1000&page=$i"
+
+    echo "Stop pidstat"
+    ssh -P 22 administrateur@$IP "pkill -f pistart"
+
+    echo "Transfert pidstat file"
+    scp -P 22 administrateur@$IP:pidstat* ./
+
+    echo "========== PIDSTAT REPORT for SSHD =========="
+    cpu_values=$(grep -a "postgres" pidstat_cpu.log | tr -s ' ' | cut -d ' ' -f 8)
+    cpu_number_value=$(echo "$cpu_values" | wc -l)
+    cpu_sum=$(echo "$cpu_values" | awk '{sum+=$1} END {print sum}')
+    cpu_mean=$(echo "$cpu_sum / $cpu_number_value" | bc -l)
+    cpu_max=$(echo "$cpu_values" | sort -nr | head -1)
+
+
+    mem_values=$(grep -a "postgres" pidstat_mem.log | tr -s ' ' | cut -d ' ' -f 7)
+    mem_number_value=$(echo "$mem_values" | wc -l)
+    mem_sum=$(echo "$mem_values" | awk '{sum+=$1} END {print sum}')
+    mem_mean=$(echo "$mem_sum / $mem_number_value" | bc -l)
+    mem_max=$(echo "$mem_values" | sort -nr | head -1)
+
+    echo "Max CPU usage for SSHD: $cpu_max %"
+    echo "Mean CPU usage for SSHD: $cpu_mean %"
+    echo "Max MEM (RSS) for SSHD: $mem_max KB"
+    echo "Mean MEM (RSS) for SSHD: $mem_mean KB"
+    echo "============================================="
 done
+
+
 
