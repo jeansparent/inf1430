aa70f512b0009d3f268070b6999b890d1c73d87c
jeansparent <jeanseb.parent@outlook.com>
2025-05-09 13:55:38 -0400

    Modif

--- Bash/Test/Applicatif/sc4-7.sh
+++ Bash/Test/Applicatif/sc4-7.sh
@@ -9,15 +9,6 @@ help=false
 # Options du script
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
-        --ip) 
-            if [[ -n "$2" && ! "$2" =~ ^-- ]]; then
-                IP="$2"
-                shift
-            else
-                echo "Erreur : --ip nécessite une valeur (ex: --ip)"
-                exit 1
-            fi
-            ;;
         --url) 
             if [[ -n "$2" && ! "$2" =~ ^-- ]]; then
                 URL="$2"
@@ -50,81 +41,8 @@ if $help; then
 fi
 
 
-echo "Cleanup VM"
-ssh -P 22 administrateur@$IP "rm -rf /home/administrateur/pid*.log"
-rm -rf /home/administrateur/pid*.log
-
-echo "Starting pidstat"
-ssh -P 22 administrateur@$IP "nohup pidstat -u 1 > pidstat_cpu.log 2>&1 &"
-pidstat_cpu_pid=$!
-ssh -P 22 administrateur@$IP "nohup pidstat -r 1 > pidstat_mem.log 2>&1 &"
-pidstat_mem_pid=$!
-
-echo "Test SC3 concurent: 10 Total: 100"
+echo "Test SC4-7 concurent: 10 Total: 100"
 ab -n 100 -c 10 "$URL"
 
-echo "Stop pidstat"
-ssh -P 22 administrateur@$IP "sudo killall pidstat"
-
-echo "Transfert pidstat file"
-scp -P 22 administrateur@$IP:pidstat* ./
-
-echo "========== PIDSTAT REPORT for python =========="
-cpu_values=$(grep -aE "python|nginx|postgres" pidstat_cpu.log | tr -s ' ' | cut -d ' ' -f 8)
-cpu_number_value=$(echo "$cpu_values" | wc -l)
-cpu_sum=$(echo "$cpu_values" | awk '{sum+=$1} END {print sum}')
-cpu_mean=$(echo "$cpu_sum / $cpu_number_value" | bc -l)
-cpu_max=$(echo "$cpu_values" | sort -nr | head -1)
-
-
-mem_values=$(grep -aE "python|nginx|postgres" pidstat_mem.log | tr -s ' ' | cut -d ' ' -f 7)
-mem_number_value=$(echo "$mem_values" | wc -l)
-mem_sum=$(echo "$mem_values" | awk '{sum+=$1} END {print sum}')
-mem_mean=$(echo "$mem_sum / $mem_number_value" | bc -l)
-mem_max=$(echo "$mem_values" | sort -nr | head -1)
-
-echo "Max CPU usage for python: $cpu_max %"
-echo "Mean CPU usage for python: $cpu_mean %"
-echo "Max MEM (RSS) for python: $mem_max KB"
-echo "Mean MEM (RSS) for python: $mem_mean KB"
-echo "============================================="
-
-echo "Cleanup VM"
-ssh -P 22 administrateur@$IP "rm -rf /home/administrateur/pid*.log"
-rm -rf /home/administrateur/pid*.log
-
-echo "Starting pidstat"
-ssh -P 22 administrateur@$IP "nohup pidstat -u 1 > pidstat_cpu.log 2>&1 &"
-pidstat_cpu_pid=$!
-ssh -P 22 administrateur@$IP "nohup pidstat -r 1 > pidstat_mem.log 2>&1 &"
-pidstat_mem_pid=$!
-
-echo "Test SC3 concurent: 10 Total: 5000"
+echo "Test SC4-7 concurent: 10 Total: 5000"
 ab -n 5000 -c 10 "$URL"
-
-echo "Stop pidstat"
-ssh -P 22 administrateur@$IP "sudo killall pidstat"
-
-echo "Transfert pidstat file"
-scp -P 22 administrateur@$IP:pidstat* ./
-
-echo "========== PIDSTAT REPORT for Application =========="
-cpu_values=$(grep -aE "python|nginx|postgres|gunicorn" pidstat_cpu.log | tr -s ' ' | cut -d ' ' -f 8)
-cpu_number_value=$(echo "$cpu_values" | wc -l)
-cpu_sum=$(echo "$cpu_values" | awk '{sum+=$1} END {print sum}')
-cpu_mean=$(echo "$cpu_sum / $cpu_number_value" | bc -l)
-cpu_max=$(echo "$cpu_values" | sort -nr | head -1)
-
-
-mem_values=$(grep -aE "python|nginx|postgres|gunicorn" pidstat_mem.log | tr -s ' ' | cut -d ' ' -f 7)
-mem_number_value=$(echo "$mem_values" | wc -l)
-mem_sum=$(echo "$mem_values" | awk '{sum+=$1} END {print sum}')
-mem_mean=$(echo "$mem_sum / $mem_number_value" | bc -l)
-mem_max=$(echo "$mem_values" | sort -nr | head -1)
-
-echo "Max CPU usage for Application: $cpu_max %"
-echo "Mean CPU usage for Application: $cpu_mean %"
-echo "Max MEM (RSS) for Application: $mem_max KB"
-echo "Mean MEM (RSS) for Application: $mem_mean KB"
-echo "============================================="
-
